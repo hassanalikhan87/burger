e7d8161d87450978143f8a28b575423f6f974bd2
Ruby Banga <uic.ruby@gmail.com>
2018-11-16 17:25:44 -0800

    heroku

--- config/connection.js
+++ config/connection.js
@@ -7,15 +7,22 @@ if(process.env.JAWSDB_URL)
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else{
+  connection = mysql.createConnection({
+    port: '3306',
+    host: 'z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'damqsd175apwd4t7',
+    password: 'nloflvhmb4bmer6n',
+    database: 'g93ao3nc9l66s777'
+  });
+};
 
-connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",  // please provide your own password here
-  database: "burgers_db"
-});
-}
+// connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",  // please provide your own password here
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
